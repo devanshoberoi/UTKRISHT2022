e2a313b2f9cab60eccaf6ef0fde8763b7681d4ff
devansh oberoi <oberoidevansh@gmail.com>
2022-09-06 12:32:42 +0530

    first commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
 const firebaseConfig = {
-    apiKey: "AIzaSyCggZCcBun0cwNfOWGC2K8pZcgIRWMfqwY",
-    authDomain: "olx-sijeesh.firebaseapp.com",
-    projectId: "olx-sijeesh",
-    storageBucket: "olx-sijeesh.appspot.com",
-    messagingSenderId: "767411886432",
-    appId: "1:767411886432:web:2ef6862afc88f2c423a605",
-    measurementId: "G-4ELNR9DJHL"
+  apiKey: "AIzaSyChsbrxmTjnEa6Kbj0qB9uQECUKm4tzTcU",
+  authDomain: "utkrisht-2022.firebaseapp.com",
+  projectId: "utkrisht-2022",
+  storageBucket: "utkrisht-2022.appspot.com",
+  messagingSenderId: "191609329945",
+  appId: "1:191609329945:web:d94622cddca601f6020d88",
+  measurementId: "G-X35TGCTFCD"
   };
 
   export const Firebase= firebase.initializeApp(firebaseConfig)//named export
